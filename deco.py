5a5285bdd04b03e5a19b1d53e948c7b6a0790bd1
Zagoidin <djsoozookee@gmail.com>
2025-10-24 08:38:36 +0400

    Update deco.py

--- deco.py
+++ deco.py
@@ -1,5 +1,7 @@
 import base64
-
+import binascii
 n = "b'304a72516d394375304b633649485a7a62334e6f6532677a6546393061444e75583249304d325239'"
 s = base64.b64decode(n).decode("utf-8")
 print(s)
+e = n.decode(utf-8)
+print(e)
